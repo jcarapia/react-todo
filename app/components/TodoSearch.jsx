a9761f0de4537df38d48e2bfcddbbc6f3ff936a9
Jorge <jcarapia83@Gmail.com>
2016-11-10 19:25:36 -0800

    added todosearch testing

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -15,7 +15,7 @@ var TodoSearch = React.createClass({
 				</div>
 				<div>
 					<label>
-						<input type="checkbox" ref="showCompleted" onChange="this.handleSearch"/>
+						<input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
 						Show Completed Todos
 					</label>	
 				</div>
